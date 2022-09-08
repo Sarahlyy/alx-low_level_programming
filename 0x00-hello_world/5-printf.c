6c4c9c184b37a82fd3497184eadac0a20927c941
Sarahlyy <saradarko22@gmail.com>
2022-09-08 09:35:26 -0700

    learning c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,30 +1,18 @@
 #include<stdio.h>
 
-
-
 /**
- *
  *  * main - Entry point
+ * 
+ * *  * Return: Always 0 (Success)
  *
- *   *
- *
- *    * Description: A C program that prints with printf function.
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *      */
-
-
+*/
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 
 }
 
