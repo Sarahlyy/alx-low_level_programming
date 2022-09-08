23fbf1eb4c411ba36045f236d9001e9492230047
Sarahlyy <saradarko22@gmail.com>
2022-09-08 07:00:35 -0700

    0-preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 int main(void)
 
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Description: A C program that prints with put function
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *      */
+
+
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
