f8517a0bc62489b1c5558c7a5527c59c7de98ca9
Sarahlyy <saradarko22@gmail.com>
2022-09-15 06:37:55 -0700

    Random

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,7 @@
 #include <time.h>
 
 /**
- *  main - Entry point
- *
- * Description: print value of n satus; zero, positive or negative 
+ *  main - Entry point 
  *
  * Return: Always 0 (Success)
 */
